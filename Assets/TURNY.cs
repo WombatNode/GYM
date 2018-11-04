f1806904f1e4d48e6d8a40d8a5da57057845b63c
Jaden Lanceman <jadenlanceman@gmail.com>
2018-11-04 21:10:11 +1100

    Initial commit

---
+++ Assets/TURNY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TURNY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        transform.position += transform.rotation * new Vector3(0, 0, Input.GetAxis("Vertical") / 3);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 10, 0);
+    }
+}
---
+++ Assets/spinner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("k")){
+            transform.Rotate(0, 0, 4);
+        }
+            
+	}
+}
